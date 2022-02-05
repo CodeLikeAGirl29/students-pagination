d8470bffa9bcd005e27969a5b5f64670cd8a4855
Lindsey Howard <miss_leading009@yahoo.com>
2022-02-05 06:27:22 -0500

    initial upload

---
+++ js/script.js
@@ -0,0 +1,88 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+function showPage(list, page) {
+   let startIndex = page * itemsPerPage - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+    
+   for (let i = 0; i < list.length; i++) {
+     let currentStudents = list[i];
+    // conditional to make list items for max 10 students 
+     if (list.indexOf(currentStudents) >= startIndex && list.indexOf(currentStudents) < endIndex) {
+        const li = document.createElement('li'); 
+        li.className = 'student-item cf'
+        li.innerHTML = `
+          <div class="student-details">
+            <img class="avatar" src=${currentStudents.picture.large} alt="Profile Picture">
+            <h3>${currentStudents.name.title} ${currentStudents.name.first} ${currentStudents.name.last}</h3>
+            <span class="email">${currentStudents.email}</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined ${currentStudents.registered.date}</span>
+          </div>
+        `;   
+        studentList.append(li);
+     } 
+   }
+  }
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+function addPagination(list) {
+   const numOfPages = Math.ceil(list.length / itemsPerPage);
+   const linkList = document.querySelector('.link-list');
+  
+   linkList.innerHTML = '';
+   
+   for(let i = 1; i <= numOfPages; i++) {
+   
+     const button = document.createElement('li');
+     button.innerHTML = `
+         <li>
+           <button type='button'>${i}</button>
+         </li>
+     `;
+   
+     linkList.append(button);
+   } 
+ // this highlights page 1 when page first loads
+   const btn1 = document.querySelector('button');
+   btn1.className = 'active';
+   console.log(btn1);
+     
+   linkList.addEventListener('click', (e) => {  
+     const targetBtn = e.target;
+     const previousBtn = document.querySelector('.active') 
+   
+     if(targetBtn.tagName === 'BUTTON'){
+       btn1.className = 'inactive';
+       previousBtn.className = 'inactive';
+       targetBtn.className = 'active';  
+       showPage(data, e.target.textContent);
+     } 
+   })
+ }   
+ // Call functions 
+ showPage(data, 1);
+ addPagination(data);
